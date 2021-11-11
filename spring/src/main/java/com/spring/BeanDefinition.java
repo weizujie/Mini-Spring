7b7f07111fc753f48b9836e0c1a90ac8590fc72c
weizujie <byojiaoxianz7@outlook.com>
2021-11-11 15:22:29 +0800

    step 1: 创建最简单的容器

--- spring/src/main/java/com/spring/BeanDefinition.java
+++ spring/src/main/java/com/spring/BeanDefinition.java
@@ -1,4 +1,4 @@
-package com.tinyioc;
+package com.spring;
 
 /**
  * @author weizujie
---
+++ spring/src/test/java/com/spring/UserService.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+/**
+ * @author weizujie
+ * @date 2021/11/11
+ */
+public class UserService {
+
+    public void queryUserInfo() {
+        System.out.println("查询用户信息");
+    }
+
+}
