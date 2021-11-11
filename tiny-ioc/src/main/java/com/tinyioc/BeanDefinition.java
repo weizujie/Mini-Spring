cf91aaab8291a40c61bcbde430e16dfef3cde5a8
weizujie <byojiaoxianz7@outlook.com>
2021-11-11 14:48:26 +0800

    [tiny-ioc] step-1-最基本的容器

---
+++ tiny-ioc/src/main/java/com/tinyioc/BeanDefinition.java
@@ -0,0 +1,18 @@
+package com.tinyioc;
+
+/**
+ * @author weizujie
+ * @date 2021/11/11
+ */
+public class BeanDefinition {
+
+    private Object bean;
+
+    public BeanDefinition(Object bean) {
+        this.bean = bean;
+    }
+
+    public Object getBean() {
+        return bean;
+    }
+}
